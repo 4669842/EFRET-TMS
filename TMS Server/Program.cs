f881df008624591b42acde6683e6544f8137206c
Dan Khosa <S4669842@bournemouth.ac.u>
2021-09-29 10:34:56 +0100

    Fixed issue where datagrid crashed on SSNA Date field.

--- TMS Server/Program.cs
+++ TMS Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Lidgren.Network;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace TMS_Server
@@ -8,12 +9,26 @@ namespace TMS_Server
     {
         static LidgrenWorkThread workerObject;
         static Thread workerThread;
-
+        static NetPeerConfiguration serverconfig;
+        static NetPeerConfiguration clientconfig;
+        static NetServer server;
+        static NetClient client;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main(string[] args)
         {
+            NetPeerConfiguration serverconfig = new NetPeerConfiguration("EFRET");
+            serverconfig.Port = 8081;
+            serverconfig.MaximumConnections = 100;
+            server = new NetServer(serverconfig);
+            Thread serverthread = new Thread(StartServer);
+            serverthread.Start();
+
+            /*
+             * 
+             * 
+             */
             bool startServer = true;
             int ScreenConfiguration = 2;
 
@@ -51,6 +66,75 @@ namespace TMS_Server
             #endregion
         }
 
+
+        static void AcceptConsoleInput()
+        {
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                NetOutgoingMessage om = client.CreateMessage(input);
+                client.SendMessage(om, NetDeliveryMethod.ReliableSequenced);
+                client.FlushSendQueue();
+            }
+
+            AcceptConsoleInput();
+        }
+
+        static void StartServer()
+        {
+            server.Start();
+            NetIncomingMessage message;
+
+            while (true)
+            {
+                message = server.WaitMessage(500);
+                if (message != null)
+                {
+                    switch (message.MessageType)
+                    {
+                        case NetIncomingMessageType.DiscoveryRequest:
+                            NetOutgoingMessage response = server.CreateMessage();
+                            response.Write((byte)1); // Do I need to do this?
+                            server.SendDiscoveryResponse(response, message.SenderEndPoint);
+                            break;
+                        case NetIncomingMessageType.DebugMessage:
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("(Server) Debug: " + message.ReadString());
+                            break;
+                        case NetIncomingMessageType.StatusChanged:
+                            NetConnectionStatus status = (NetConnectionStatus)message.ReadByte();
+
+                            string reason = message.ReadString();
+                            Console.WriteLine(NetUtility.ToHexString(message.SenderConnection.RemoteUniqueIdentifier) + " " + status + ": " + reason);
+
+                            if (status == NetConnectionStatus.Connected)
+                                Console.WriteLine("Remote hail: " + message.SenderConnection.RemoteHailMessage.ReadString());
+                            break;
+                        case NetIncomingMessageType.Data:
+                            // incoming chat message from a client
+                            string chat = message.ReadString();
+
+
+                            // broadcast this to all connections, except sender
+                            List<NetConnection> all = server.Connections; // get copy
+                                                                          //all.Remove(message.SenderConnection);
+                            Console.WriteLine(all + "hello ");
+
+                            NetOutgoingMessage om = server.CreateMessage();
+                            server.SendMessage(om, all, NetDeliveryMethod.ReliableOrdered, 0);
+
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("(Server) Unrecognized message type! (" + message.MessageType + ")");
+                            break;
+                    }
+                }
+                server.Recycle(message);
+            }
+            Thread.Sleep(1);
+        }
         static void game_Exiting(object sender, EventArgs e)
         {
             if (workerObject != null)
